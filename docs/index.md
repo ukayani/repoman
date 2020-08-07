dedc9caf8eea405a2eabe2767652aa04f732bc41
ukayani <kayani.u@gmail.com>
2020-08-07 19:01:51 -0400

    - add sample code to docs

--- docs/index.md
+++ docs/index.md
@@ -46,3 +46,45 @@ $ npm i @ukayani/repoman --save
 
 ## Sample Code
 
+Below is an automation which reads a list of repos from a file
+and then removes all Maven Wrapper files from them. It then creates a PR
+for each change and optionally merges the PR.
+
+```typescript
+import { Config, GitHub } from "@ukayani/repoman";
+
+async function main() {
+  const github = await GitHub.init();
+  const config = await Config.load("services.yaml");
+  const repos = await github.getRepositories(config.repos);
+  const automerge = false;
+
+  for (const repo of repos) {
+    const result = await repo
+      .checkout("maven-wrapper-removal")
+      .deleteFile(".mvn")
+      .deleteFile("mvnw")
+      .deleteFile("mvnw.cmd")
+      .commit("Remove maven wrapper");
+    console.log(`Changes for ${repo.name}`);
+    console.log(result.changelog());
+
+    if (result.hasChanges()) {
+      const pr = await repo.createPullRequest(
+        result.branch,
+        "PR Title",
+        "body",
+        ["ukayani"]
+      );
+      if (automerge) {
+        await repo.mergePullRequest(pr);
+      }
+      console.log(pr);
+    }
+  }
+}
+
+main().catch(console.error);
+
+```
+
--- examples/test.ts
+++ examples/test.ts
@@ -1,21 +1,33 @@
-import { Config, FS, GitHub } from "../src";
+import { Config, GitHub } from "../src";
 
 async function main() {
   const github = await GitHub.init();
   const config = await Config.load("services.yaml");
-  const mvn = await FS.getFile(".mvn");
-  const mvnw = await FS.getFile("mvnw");
-  const mvnwcmd = await FS.getFile("mvnw.cmd");
   const repos = await github.getRepositories(config.repos);
+  const automerge = false;
 
   for (const repo of repos) {
     const result = await repo
-      .checkout("maven-wrapper-test")
-      .addLocalFiles(mvn)
-      .addLocalFiles(mvnw)
-      .addLocalFiles(mvnwcmd)
-      .commit("Updating to latest maven");
+      .checkout("maven-wrapper-removal")
+      .deleteFile(".mvn")
+      .deleteFile("mvnw")
+      .deleteFile("mvnw.cmd")
+      .commit("Remove maven wrapper");
+    console.log(`Changes for ${repo.name}`);
     console.log(result.changelog());
+
+    if (result.hasChanges()) {
+      const pr = await repo.createPullRequest(
+        result.branch,
+        "PR Title",
+        "body",
+        ["ukayani"]
+      );
+      if (automerge) {
+        await repo.mergePullRequest(pr);
+      }
+      console.log(pr);
+    }
   }
 }
 
